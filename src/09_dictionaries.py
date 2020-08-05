85e7980eb0f519034de2b6f61879e7add5a842f8
Michael Rockingham <mrockingham2@gmail.com>
2020-08-05 16:56:07 -0400

    mvp

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -36,6 +36,13 @@ waypoints = [
 # Add a new waypoint to the list
 # YOUR CODE HERE
 
+waypoints.append({
+    "lat": 82,
+    "lon": 135,
+    "name": "Antarctica"
+})
+print(waypoints)
+
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
@@ -43,5 +50,12 @@ waypoints = [
 
 # YOUR CODE HERE
 
+waypoints[0]["lon"] = -130
+waypoints[0]["name"] = "not a real place"
+
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for n in waypoints:
+    for key, value in n.items():
+        print(value)
--- src/11_args.py
+++ src/11_args.py
@@ -6,6 +6,9 @@
 
 # YOUR CODE HERE
 
+def f1(a,b):
+    return a + b
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -13,6 +16,13 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*a):
+    total=0
+
+    for i in a:
+        total += 1
+
+        return total
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -31,6 +41,9 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+def f3(a, b=1):
+    return a + b
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,22 +57,27 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f'key: {key}, value: {value}')
+
 
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-f4(a=12, b=30)
+    f4(a=12, b=30)
 
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
-    "monster": "goblin",
-    "hp": 3
-}
+    f4(city="Berkeley", population=121240, founded="March 23, 1868")
+
+    d = {
+        "monster": "goblin",
+        "hp": 3
+    }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+    f4(**d)
